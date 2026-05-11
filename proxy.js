6a9b9070becc1fa6ccb17439d96b8368ae32fe57
shruti0101 <shrutiguptacu@gmail.com>
2026-05-11 10:39:11 +0530

    admin token issue fix

--- proxy.js
+++ proxy.js
@@ -4,7 +4,6 @@ export function proxy(req) {
   
   const token = req.cookies.get("adminToken")?.value;
 
-  // protect all admin routes
   if (req.nextUrl.pathname.startsWith("/admin")) {
     if (!token) {
       return NextResponse.redirect(new URL("/login", req.url));
